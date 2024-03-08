96faaebc6e1aa7be17b96ae0836d1612c941e2f2
Ricardo Miguel R <ricardomr.23@hotmail.com>
2024-03-08 18:55:38 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let numbersCard = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  let sticksCard = [
+    { Symbol: "♦", color: "red" },
+    { Symbol: "♥", color: "red" },
+    { Symbol: "♠", color: "black" },
+    { Symbol: "♣", color: "black" }
+  ];
+
+  document.querySelector(".number").innerHTML =
+    numbersCard[Math.floor(Math.random() * numbersCard.length)];
+  let sticksCard2 = sticksCard[Math.floor(Math.random() * sticksCard.length)];
+  document.querySelectorAll(".topstick").forEach((elem, index) => {
+    elem.innerHTML = sticksCard2.Symbol;
+    elem.style.color = sticksCard2.color;
+  });
 };
